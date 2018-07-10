85ae40304e89fa4e4139c1f0cf0fc4af823ec138
shasaur <shasaur@hotmail.co.uk>
2018-07-10 22:00:24 +0100

    Fixed paths

--- core/filepaths.py
+++ core/filepaths.py
@@ -1,7 +1,8 @@
 import sys
 
 def get_filepath(file):
-    if sys.platform == "win32":
-        return "../../data/new/" + file
+    print(sys.platform)
+    if not sys.platform == "win32":
+        return "../data/" + file
     else:
-        return "..\\data\\new\\"+ file
+        return "..\\..\\data\\new\\"+ file
